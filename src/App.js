e1a0cfb88644e48b0a11216ea5a1574cb69c20a4
Lee Drinkwater <drinkwater32@gmail.com>
2024-02-08 18:04:23 -0600

    adjusting text on the home page test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello, World!</h1>
+        <h1>Lee's Weather App</h1>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('app renders with proper title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = screen.getByText("Lee's Weather App");
+  expect(appTitle).toBeInTheDocument();
 });
