d986a429f2b1e36433ef10f126444cd3e352fdfe
Lee Drinkwater <drinkwater32@gmail.com>
2024-02-08 19:03:36 -0600

    delete manifest

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MainPage from "./components/MainPage/MainPage";
 
 function App() {
   return (
-    <div style={{ backgroundColor: "#1e90ff", minHeight: "100vh" }}>
+    <div>
       <MainPage />
     </div>
   );
