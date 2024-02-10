18bbdcadb5062db9cc779ea21ba64cd5c69f0807
Lee Drinkwater <drinkwater32@gmail.com>
2024-02-10 15:52:39 -0600

    adding backend.js because it uses an environment variable so key is not seen... the solution is actually that the front end doesnt see the key so it cant be viewed in the app bundle

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# basic_weather_app
+# Basic Weather Application
+
 I know, I know... ANOTHER weather app? Boo!!!! Yea I get it. But this isn't for you, it's for me and my learning :D
+
+# Weather Data
+
+blah blah scraped with API, info about there
+
+# How to Get an API Key
+
+info about getting api key, check out https://home.openweathermap.org/
+
+# Hiding API Key with A Mini Backend
+
+info about creating a mini back end
+
+I used this video for guidance: https://www.youtube.com/watch?v=FcwfjMebjTU
---
+++ backend.js
@@ -0,0 +1,53 @@
+// backend.js
+
+// Used as a mini backend so API key can be grabbed from an environment variable without being shown in the app bundle
+
+// Define Port
+const PORT = 8000;
+
+// Import Modules Needed
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+// Initialize Backend
+const backend = express();
+
+// Allow cross origin requests
+backend.use(cors());
+
+// Proof of concept for backend working (visit localhost port defined above)
+backend.get("/", (req, res) => {
+  res.json("welcome to the backend! :)");
+});
+
+// API Call for getWeather
+backend.get("/getWeather", (req, res) => {
+  const city = req.query.city;
+  if (!city) {
+    return res.status(400).json({ error: "no city" });
+  }
+
+  const options = {
+    method: "GET",
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      process.env.REACT_APP_OPENWEATHER_API_KEY, // Add your own .env file to your root folder, REACT_APP_OPENWEATHER_API_KEY=YOURKEYHERE (no quotes)
+  };
+
+  // Request Response and handle any errors
+  axios
+    .request(options)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+// Spin up backend with confirmation of where it's running
+backend.listen(PORT, () => console.log(`Backend is running on port ${PORT}`));
