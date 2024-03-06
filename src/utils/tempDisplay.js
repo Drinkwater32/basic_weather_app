af8d6d519fc3ac2d3bb039e64b17b0f6e9ecbc6f
Drinkwater32 <drinkwater32@gmail.com>
2024-03-05 19:43:26 -0600

    add function that converts K to F and adds degree symbol, click it in to results display

--- src/utils/tempDisplay.js
+++ src/utils/tempDisplay.js
@@ -1,3 +1,9 @@
 //tempDisplay.js
 
 // function to convert temps from K to F, and add degrees symbol for display
+
+export function tempDisplay(tempKelvin) {
+  const tempFahrenheit = ((tempKelvin - 273.15) * 9) / 5 + 32;
+
+  return `${Math.round(tempFahrenheit)}°F`;
+}
